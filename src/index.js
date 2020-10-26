81b03e4d80bb917e1b064afcefc52669b2f1d54f
Darshi-R <67880153+Darshi-R@users.noreply.github.com>
2020-10-26 01:19:28 -0400

    Updated js file for current day & time

--- src/index.js
+++ src/index.js
@@ -3,8 +3,23 @@ let degreeElement=document.querySelector("#degree");
 let humidElement=document.querySelector("#humid");
 let airElement=document.querySelector("#air");
 let descriptionElement=document.querySelector("#description");
+let dateTime=document.querySelector("#dateandtime");
 
 
+function weekDays(timestamp){
+    let date=new Date(timestamp);
+    let day = ["Sunday", "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    let days= day[date.getDay()];
+    let hours=date.getHours();
+    if (hours<10){
+        hours = `0${hours}`;
+    }
+    let minutes=date.getMinutes();
+        if (minutes<10){
+        minutes = `0${minutes}`;
+    }
+    return `${days},${hours}:${minutes}`;
+}
 
 
 
@@ -14,5 +29,6 @@ degreeElement.innerHTML= Math.round(response.data.main.temp);
 humidElement.innerHTML= Math.round(response.data.main.humidity);
 airElement.innerHTML= Math.round(response.data.wind.speed);
 descriptionElement.innerHTML=(response.data.weather[0].description);
+dateTime.innerHTML= weekDays(response.data.dt*1000);
 }
 axios.get(apiUrl).then(temperature);
