02b240e6acb7036372e0175e4d8b7908416640fd
Darshi-R <67880153+Darshi-R@users.noreply.github.com>
2020-11-03 10:51:45 -0500

    Added Search Engine for city weather

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
-let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=New York&appid=32669c9d2438b0a7d99b979086328197&units=metric`;
+
+function cityApiSearch(city){
+      let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=32669c9d2438b0a7d99b979086328197&units=metric`;
+      axios.get(apiUrl).then(temperature);
+
+}
+ 
+
+let form = document.querySelector("#form-input");
+form.addEventListener("submit",citySearch);
+cityApiSearch("New york");
+function citySearch(event){
+    event.preventDefault();
+    let cityNameElement = document.querySelector("#cityCalling");
+    cityApiSearch(cityNameElement.value );
+    console.log(cityNameElement);
+}
+
 let degreeElement=document.querySelector("#degree");
 let humidElement=document.querySelector("#humid");
 let airElement=document.querySelector("#air");
 let descriptionElement=document.querySelector("#description");
 let dateTime=document.querySelector("#dateandtime");
 let imgElement= document.querySelector("#imgIcon");
-let cityElement=document.querySelector("#city");
+
 function weekDays(timestamp){
     let date=new Date(timestamp);
     let day = ["Sunday", "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
@@ -21,11 +38,10 @@ function weekDays(timestamp){
     return `${days},${hours}:${minutes}`;
     
 }
-
-
-
 function temperature (response){
 // console.log(response.data.main.temp);
+let cityElement=document.querySelector("#city");
+cityElement.innerHTML= (response.data.name);
 degreeElement.innerHTML= Math.round(response.data.main.temp);
 humidElement.innerHTML= Math.round(response.data.main.humidity);
 airElement.innerHTML= Math.round(response.data.wind.speed);
@@ -36,8 +52,4 @@ let altpath= (response.data.weather[0].description);
 // console.log(imgpath);
 imgElement.setAttribute("src",`http://openweathermap.org/img/wn/${imgpath}@2x.png`);
 imgElement.setAttribute("alt",`http://openweathermap.org/img/wn/${altpath}@2x.png`);
-
 }
-
-
-axios.get(apiUrl).then(temperature);
